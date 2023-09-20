a63bb4d8ffc11d3c5401074d135f0a430b4e5387
patrickr11 <186513@novacollege.nl>
2023-09-20 11:02:34 +0200

    gestart met de home pagina

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="manufacturers_index.php">manufacturers_index
+        ga naar fabrikanten
+    </a>
+</body>
+</html>
+
+
